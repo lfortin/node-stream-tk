d9b8f5e1a7810589470034b5f71d31adddcee6da
Laurent Fortin <laurent.fortin@gmail.com>
2013-11-12 20:01:11 -0500

    added files

---
+++ spec-runner.js
@@ -0,0 +1,120 @@
+
+
+var assert = require('assert'),
+    domain = require('domain'),
+    os = require('os'),
+    stream = require('stream'),
+    stk = require('./stream-tk');
+
+function getEOL(n) {
+  var lines = [];
+  for(var i = 0; i < n; i++) {
+    lines.push(os.EOL);
+  }
+  return lines.join('');
+}
+
+process.stdout.write("running tests..." + getEOL(2));
+
+
+var tester = domain.create();
+
+tester.on('error', function(error) {
+  process.stderr.write(error + getEOL(2));
+  process.exit();
+});
+
+
+tester.run(function() {
+
+  // API signature
+  assert.ok(stk.isStream, ".isStream() method expected");
+  assert.ok(stk.isReadable, ".isReadable() method expected");
+  assert.ok(stk.isWritable, ".isWritable() method expected");
+  assert.ok(stk.isTransform, ".isTransform() method expected");
+  //assert.ok(stk.isPassThrough, ".isPassThrough() method expected");
+  assert.ok(stk.isFlowing, ".isFlowing() method expected");
+  //assert.ok(stk.pipeOn, ".pipeOn() method expected");
+  //assert.ok(stk.pipeBy, ".pipeBy() method expected");
+  //assert.ok(stk.bufferize, ".bufferize() method expected");
+  //assert.ok(stk.streamize, ".streamize() method expected");
+  //assert.ok(stk.compose, "compose() method expected");
+  //assert.ok(stk.extend, ".extend() method expected");
+  assert.ok(stk.dev.Null, ".dev.Null() method expected");
+  assert.ok(stk.dev.Zero, ".dev.Zero() method expected");
+  assert.ok(stk.dev.Full, ".dev.Full() method expected");
+  assert.ok(stk.dev.Random, ".dev.Random() method expected");
+  //assert.ok(monitor.whatever, "whatever expected");
+
+  process.stdout.write("API signature OK" + getEOL(1));
+
+
+  // test methods
+  var readable  = new stream.Readable(),
+      writable  = new stream.Writable(),
+      duplex    = new stream.Duplex(),
+      transform = new stream.Transform(),
+      through   = new stream.PassThrough(),
+      obj       = {};
+
+  readable._read = function(){};
+  duplex._read = function(){};
+
+  assert.deepEqual(stk.isStream(readable),  true,  "stk.isStream(readable) : true expected");
+  assert.deepEqual(stk.isStream(writable),  true,  "stk.isStream(writable) : true expected");
+  assert.deepEqual(stk.isStream(duplex),    true,  "stk.isStream(duplex) : true expected");
+  assert.deepEqual(stk.isStream(transform), true,  "stk.isStream(transform) : true expected");
+  assert.deepEqual(stk.isStream(through),   true,  "stk.isStream(through) : true expected");
+  assert.deepEqual(stk.isStream(obj),       false, "stk.isStream(obj) : false expected");
+
+  assert.deepEqual(stk.isReadable(readable),  true,  "stk.isReadable(readable) : true expected");
+  assert.deepEqual(stk.isReadable(writable),  false, "stk.isReadable(writable) : false expected");
+  assert.deepEqual(stk.isReadable(duplex),    true,  "stk.isReadable(duplex) : true expected");
+  assert.deepEqual(stk.isReadable(transform), true,  "stk.isReadable(transform) : true expected");
+  assert.deepEqual(stk.isReadable(through),   true,  "stk.isReadable(through) : true expected");
+  assert.deepEqual(stk.isReadable(obj),       false, "stk.isReadable(obj) : false expected");
+
+  assert.deepEqual(stk.isWritable(readable),  false, "stk.isWritable(readable) : false expected");
+  assert.deepEqual(stk.isWritable(writable),  true,  "stk.isWritable(writable) : true expected");
+  assert.deepEqual(stk.isWritable(duplex),    true,  "stk.isWritable(duplex) : true expected");
+  assert.deepEqual(stk.isWritable(transform), true,  "stk.isWritable(transform) : true expected");
+  assert.deepEqual(stk.isWritable(through),   true,  "stk.isWritable(through) : true expected");
+  assert.deepEqual(stk.isWritable(obj),       false, "stk.isWritable(obj) : false expected");
+
+  assert.deepEqual(stk.isTransform(readable),  false, "stk.isTransform(readable) : false expected");
+  assert.deepEqual(stk.isTransform(writable),  false, "stk.isTransform(writable) : false expected");
+  assert.deepEqual(stk.isTransform(duplex),    false, "stk.isTransform(duplex) : false expected");
+  assert.deepEqual(stk.isTransform(transform), true,  "stk.isTransform(transform) : true expected");
+  assert.deepEqual(stk.isTransform(through),   true,  "stk.isTransform(through) : true expected");
+  assert.deepEqual(stk.isTransform(obj),       false, "stk.isTransform(obj) : false expected");
+
+  assert.deepEqual(stk.isFlowing(readable),  false, "stk.isFlowing(readable) : false expected");
+  assert.deepEqual(stk.isFlowing(writable),  false, "stk.isFlowing(writable) : false expected");
+  assert.deepEqual(stk.isFlowing(duplex),    false, "stk.isFlowing(duplex) : false expected");
+  assert.deepEqual(stk.isFlowing(transform), false, "stk.isFlowing(transform) : false expected");
+  assert.deepEqual(stk.isFlowing(through),   false, "stk.isFlowing(through) : false expected");
+  assert.deepEqual(stk.isFlowing(obj),       false, "stk.isFlowing(obj) : false expected");
+
+  readable.resume();
+  duplex.resume();
+  transform.resume();
+  through.resume();
+
+  assert.deepEqual(stk.isFlowing(readable),  true, "stk.isFlowing(readable) : true expected");
+  assert.deepEqual(stk.isFlowing(duplex),    true, "stk.isFlowing(duplex) : true expected");
+  assert.deepEqual(stk.isFlowing(transform), true, "stk.isFlowing(transform) : true expected");
+  assert.deepEqual(stk.isFlowing(through),   true, "stk.isFlowing(through) : true expected");
+
+  readable.pause();
+  duplex.pause();
+  transform.pause();
+  through.pause();
+
+  assert.deepEqual(stk.isFlowing(readable),  false, "stk.isFlowing(readable) : false expected");
+  assert.deepEqual(stk.isFlowing(duplex),    false, "stk.isFlowing(duplex) : false expected");
+  assert.deepEqual(stk.isFlowing(transform), false, "stk.isFlowing(transform) : false expected");
+  assert.deepEqual(stk.isFlowing(through),   false, "stk.isFlowing(through) : false expected");
+
+  process.stdout.write("test methods OK" + getEOL(1));
+});
+
---
+++ stream-tk.js
@@ -0,0 +1,106 @@
+// Toolkit for handling Node.js data streams
+
+// Copyright (c) 2013 Laurent Fortin
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+var stream   = require('stream'),
+    fs       = require('fs');
+
+
+var xstream = {};
+
+xstream.version = '0.0.1';
+
+
+xstream.isStream = function isStream(obj) {
+  return obj instanceof stream.Readable || obj instanceof stream.Writable;
+};
+
+xstream.isReadable = function isReadable(obj) {
+  return this.isStream(obj) && !!obj.readable;
+};
+
+xstream.isWritable = function isWritable(obj) {
+  return this.isStream(obj) && !!obj.writable;
+};
+
+xstream.isTransform = function isTransform(obj) {
+  return obj instanceof stream.Transform;
+};
+
+xstream.isFlowing = function isFlowing(obj) {
+  return this.isReadable(obj) && !!obj._readableState.flowing;
+};
+
+xstream.dev = {};
+
+xstream.dev.Null = function Null(mode) {
+  if(mode === "read") {
+    return fs.createReadStream("/dev/null");
+  }
+  if(mode === "write") {
+    return fs.createWriteStream("/dev/null");
+  }
+  throw new Error("mode must me either 'read or 'write'");
+};
+
+xstream.dev.Zero = function Zero(mode, length) {
+  if(mode === "read") {
+    if(!Number(length)) {
+      throw new Error("must provide length in bytes");
+    }
+    return fs.createReadStream("/dev/zero", {start: 0, end: length-1});
+  }
+  if(mode === "write") {
+    return fs.createWriteStream("/dev/zero");
+  }
+  throw new Error("mode must me either 'read or 'write'");
+};
+
+xstream.dev.Full = function Full(mode, length) {
+  if(mode === "read") {
+    if(!Number(length)) {
+      throw new Error("must provide length in bytes");
+    }
+    return fs.createReadStream("/dev/full", {start: 0, end: length-1});
+  }
+  if(mode === "write") {
+    return fs.createWriteStream("/dev/full");
+  }
+  throw new Error("mode must me either 'read or 'write'");
+};
+
+xstream.dev.Random = function Random(mode, length) {
+  if(mode === "read") {
+    if(!Number(length)) {
+      throw new Error("must provide length in bytes");
+    }
+    return fs.createReadStream("/dev/urandom", {start: 0, end: length-1});
+  }
+  if(mode === "write") {
+    return fs.createWriteStream("/dev/urandom");
+  }
+  throw new Error("mode must me either 'read or 'write'");
+};
+
+module.exports = xstream;
+
